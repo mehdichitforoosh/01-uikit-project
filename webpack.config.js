a451817b3e85f13a739eaf8cc3dfa8e5bde69ebd
mehdichitforoosh <mehdichitforoosh@gmail.com>
2018-08-03 16:35:56 +0430

    Add index.html,index.js,..

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 var path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './app/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                // use https://www.npmjs.com/package/babel-loader for ES6 to ES5 like import in js
+              loader: 'babel-loader',
+              options: {
+                presets: ['env']
+              }
+            }
+          }
+        ]
+      }
 };
